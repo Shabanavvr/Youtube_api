5852a6fae31ae544b3ab17bc377762754cd04f71
Shabana <92933268+Shabanavvr@users.noreply.github.com>
2024-03-31 23:41:32 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+from googleapiclient.discovery import build
+import matplotlib.pyplot as plt
+
+
+api_key = 'xxxxxxxxxxxxxxxx'
+
+
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+
+request = youtube.channels().list(part='statistics', forUsername='shabanafathimascse4754')
+response = request.execute()
+
+
+print(response)
+
+
+statistics = response['items'][0]['statistics']
+subscriber_count = int(statistics['subscriberCount'])
+view_count = int(statistics['viewCount'])
+video_count = int(statistics['videoCount'])
+
+
+print('Subscriber Count:', subscriber_count)
+print('Total Views:', view_count)
+print('Total Videos:', video_count)
+
+
+labels = ['Subscribers', 'Total Videos']
+sizes = [subscriber_count, video_count]
+colors = ['lightblue', 'lightgreen']
+explode = (0.1, 0) 
+
+plt.figure(figsize=(8, 6))
+plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal') 
+plt.title('YouTube Channel Statistics')
+plt.show()
